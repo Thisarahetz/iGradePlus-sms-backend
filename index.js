4bdbd49cf8a251d9a54f634706067ddafbc749a1
[Thisara Hettikankanama] <[thisarayashodagcc@gmail.com]>
2022-03-23 15:47:18 +0530

    project init and login & Register

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const auth = require('./routes/auth'); //Register and login authontication
+const usersRouter = require('./routes/users'); // update and delete and getAll and find
+
+const cors = require('cors');
+//Enable CORE
+app.use(cors());
+dotenv.config();
+
+/*
+Run Server
+*/
+app.listen(8090,() => {
+    console.log("backend server is working");
+})
+
+/*  
+connect mongoose server
+*/
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+})
+.then(()=>console.log("DB Connection Successfull!!!!"))
+.catch((err)=>console.log(err));
+/*Accesp JSON*/
+app.use(express.json());
+
+/*
+Routers
+*/
+app.use("/api/auth",auth);
+app.use("/api/users",usersRouter);
+
