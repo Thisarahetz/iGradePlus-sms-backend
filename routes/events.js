8828295fa6daf37013528de2f2b5c6b17f09702a
Asanka Gamage <asankagamage1998@gmail.com>
2022-04-16 17:27:27 +0530

    retrive crud operation created

--- routes/events.js
+++ routes/events.js
@@ -20,34 +20,67 @@ router.post("/",async(req,res)=>{
 *@UPDATE
 */
 router.put("/:id", async(req,res)=>{
-    try{
-         const updateEvents = await Event.findByIdAndUpdate(
-             req.params.id,
-             {
-                 $set:req.body,
-             },
-             { new:true }
-         );
-        res.status(201).json(updateEvents);
-    }catch(err){
-        res.status(500).json(err);
-    }
+            try{
+                 const updateEvents = await Event.findByIdAndUpdate(
+                     req.params.id,
+                     {
+                         $set:req.body,
+                     },
+                     { new:true }
+                 );
+                res.status(201).json(updateEvents);
+            }catch(err){
+                res.status(500).json(err);
+            }
 
 });
+
 /*
 *@DELETE
 */
 router.delete("/:id",async(req,res)=>{
     
+            try{
+                  await Event.findByIdAndDelete(req.params.id);
+                 
+                    //res.status(201).json("Delete Events!!");
+                 
+                 //res.status(201).json("Event is not found");
+            }catch(err){
+                res.status(500).json(err);
+            }
+   
+
+});
+
+/*
+*@Find Events
+*/
+router.get("/find/:id", async(req,res)=>{
+            try{
+                 const getEvents =await Event.findById(
+                     req.params.id
+                 );
+                res.status(201).json(getEvents);
+            }catch(err){
+                res.status(500).json(err);
+            }
+
+});
+
+
+
+/*
+*@Find All Events
+*/
+router.get("/", async(req,res)=>{
     try{
-          await Event.findByIdAndDelete(req.params.id);
-         
-            res.status(201).json("Delete Events!!");
-         
-         //res.status(201).json("Event is not found");
+         const getEvents =await Event.find();
+        res.status(201).json(getEvents);
     }catch(err){
         res.status(500).json(err);
     }
 
-
 });
+
+module.exports = router
